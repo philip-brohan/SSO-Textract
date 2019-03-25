70efb76ce99f75c684732be22570aac36a15c053
Philip Brohan <philip@brohan.org>
2019-03-25 16:37:59 +0000

    Completed the image splitter script.

--- analyses/pdf_to_images/convert_1916.py
+++ analyses/pdf_to_images/convert_1916.py
@@ -5,6 +5,8 @@
 
 import subprocess
 import os
+import glob
+from PIL import Image
 
 source_file="../../originals/1916.pdf"
 if not os.path.isfile(source_file):
@@ -15,9 +17,32 @@ if not os.path.isdir(image_dir):
     os.makedirs(image_dir)
 
 # Split the PDF into pages
-proc = subprocess.Popen("pdfseparate %s %s/page_%%04d.pdf" % 
+proc = subprocess.Popen("convert -density 300 %s %s/page_%%04d.jpg" % 
                             (source_file,image_dir),
                        stdout=subprocess.PIPE, 
                        stderr=subprocess.PIPE, 
                        shell=True)
 (out, err) = proc.communicate()
+print(err)
+
+# Split the pages into quarters
+pages=glob.glob("%s/page_????.jpg" % image_dir)
+
+for page in pages:
+    pn=int(page[-8:-4])
+    if pn<5 or pn>29: continue
+    im=Image.open(page)
+    w, h = im.size
+    area = (w/10, 0, w/2*1.03, h/2*1.1)
+    sub_im=im.crop(area)
+    sub_im.save("%s_tl.jpg" % page[:-4])
+    area = (w/2*1.03, 0, w, h/2*1.1)
+    sub_im=im.crop(area)
+    sub_im.save("%s_tr.jpg" % page[:-4])
+    area = (w/10, h/2*1.1, w/2*1.03, h)
+    sub_im=im.crop(area)
+    sub_im.save("%s_bl.jpg" % page[:-4])
+    area = (w/2*1.03, h/2*1.1, w, h)
+    sub_im=im.crop(area)
+    sub_im.save("%s_br.jpg" % page[:-4])
+   
